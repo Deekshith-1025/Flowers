ac93c64f79ecad379209a69f69e493eff24718e1
Deekshith Eppakayala <edeekshith2004@gmail.com>
2025-07-26 09:48:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ uploaded_file = st.file_uploader("Upload Image", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_container_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=False)
 
     with st.spinner("Predicting..."):
         img_tensor = preprocess_image(image)
